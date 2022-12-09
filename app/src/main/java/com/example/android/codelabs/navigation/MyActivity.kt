3828e2ae9f80510e7d1c040b9a88cae3cde33ab8
Gilmarcos Santos <GILMARCOSXSANTOS@GMAIL.COM>
2022-12-08 22:58:36 -0300

    Salvar

---
+++ app/src/main/java/com/example/android/codelabs/navigation/MyActivity.kt
@@ -0,0 +1,11 @@
+package com.example.android.codelabs.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.my_activity)
+    }
+}
